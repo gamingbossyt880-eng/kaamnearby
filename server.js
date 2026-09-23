c5a90a25e2fe95abedfb9dd2327b62ae69962e6b
gamingbossyt880-eng <gamingbossyt880@gmail.com>
2026-09-24 01:06:39 +0530

    Update server.js

--- server.js
+++ server.js
@@ -11,8 +11,6 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 require("dotenv").config();
 
-app.use(express.json());
-
 // ===============================
 // CORS
 // ===============================
